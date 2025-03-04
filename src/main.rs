9e99179b2f263062671c9ebbccafa812b7dec7a4
Gabriel Arruda <gabriel@gdarruda.me>
2025-03-04 23:56:56 +0000

    Adding create and read tables

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,44 @@
 mod index;
 mod csv;
+use std::time::SystemTime;
+use uuid::Uuid;
 
 use std::fs::File;
 
+
 fn main() -> std::io::Result<()> {
 
-    let filename = "resources/sample.csv";
+    let filename = "/home/gdarruda/Projects/sandbox/clients.csv";
     let mut file = File::open(filename)?;
-    let mut tree = index::btree::BTree::create(3, "btree_files");
-
+    let mut tree = index::btree::BTree::create(1000, "/home/gdarruda/btree_files");
     csv::index_file(&file, &mut tree);
 
-    let key_value = "71";
+    let tree = index::btree::BTree::load("/home/gdarruda/btree_files");
 
-    match tree.search(key_value) {
-        None => {println!("Key not found {}", key_value)},
-        Some(key) => {
-            match csv::read_line(&mut file, key.position) {
-                Err(e) => {println!("Error: {}", e)},
-                Ok(line) => {println!("Found line: {}", line)}
+    for uuid in (0..1000).map(|_| Uuid::new_v4().to_string()) {
+
+        let now = SystemTime::now();
+
+        match tree.search(&uuid) {
+            None => {},
+            Some(key) => {
+                match csv::read_line(&mut file, key.position) {
+                    Err(e) => {println!("Error: {}", e)},
+                    Ok(line) => {println!("Found line: {}", line)}
+                }
+            }
+        };
+
+        match now.elapsed() {
+            Ok(elapsed) => {
+                println!("{}", elapsed.as_micros());
+            }
+            Err(e) => {
+                println!("Error: {e:?}");
             }
         }
-    };
+
+    }
 
     Ok(())
 }
